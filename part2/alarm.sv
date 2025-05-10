6c6113c594d3b6ef56bde42af2eee430b7609354
JvalaDeus <akhilsubbarao98@gmail.com>
2025-05-09 22:55:27 -0700

    finished part2 code(?)

--- part2/alarm.sv
+++ part2/alarm.sv
@@ -1,1 +1,17 @@
+// CSE140 lab 2  
+// How does this work? How long does the alarm stay on? 
+// (buzz is the alarm itself)
+module alarm(
+  input[6:0]   tmin,
+               amin,
+			   thrs,
+			   ahrs,
+               tday,
+               aday,						 
+  output logic buzz
+);
 
+  always_comb begin
+    buzz = ((tmin == amin) && (thrs == ahrs) && (tday == aday));
+  end
+endmodule
